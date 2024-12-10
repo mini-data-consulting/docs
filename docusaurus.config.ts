d8775d62dee85919d5cde71e8460b1d4200cdb22
Satyajeet Jadhav <satyajeetjadhav@gmail.com>
2024-12-10 22:24:30 +0530

    switching to periscope domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://mini-data-consulting.github.io",
+  url: "https://docs.periscopechat.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/docs/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
