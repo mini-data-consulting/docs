89d20faea3393b7eab0512066bb46a73ae561fb1
Satyajeet Jadhav <satyajeetjadhav@gmail.com>
2024-12-10 21:58:37 +0530

    switching to /docs/index.html/ as base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: "https://docs.periscopechat.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/docs/index.html/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
